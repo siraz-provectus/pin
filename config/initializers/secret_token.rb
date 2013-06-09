4d7cdac26fe60a90e9d531500c734eb750e0f8b3
siraz <siraz@yandex.ru>
2013-06-09 12:06:07 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PinClone::Application.config.secret_token = 'ecc190b0c0448950b9def08c8f8594748c1b38563dbb2aa4f3483588a4a0e6c12e25ab9a9b3f64d55863bf660de3fc1efc4a2244b4a78324fee7a2891ecca178'
