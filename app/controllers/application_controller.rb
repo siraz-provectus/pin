81d56e1060aa49d59fd68b7352f898419e13a009
RuslanHamidullin <ror_developer@mail.ru>
2013-06-13 15:23:36 +0400

    Some fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 #encoding: utf-8
 class ApplicationController < ActionController::Base
-  before_filter :best_user
-  #before_filter :authenticate_user!
+  
   protect_from_forgery
 
+  before_filter :get_best_users
+
   private
-  	def best_user
-  		 @best_users =User.all.sort! { |a,b| b.pins.count <=> a.pins.count }
-  	end
+
+	def get_best_users
+		@best_users = User.all.sort! { |a, b| b.pins.count <=> a.pins.count }
+	end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 #encoding: utf-8
 class HomeController < ApplicationController	
 	def index
-		#@users = User.all
 		@pins = Pin.all
 	end
 end
